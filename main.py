8969480bb2ac914b64b939a475adf84483ef3655
miizumi <76993694+miizumi@users.noreply.github.com>
2021-03-07 19:38:58 +0800

    V3

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print ('我是zeki 我好帥 快幫我介紹對象')
-print("okokokokokokokokokokokokok")
+
+def func1(apple):
+    print (apple)
+
+func1("i fuck you")
+
+def func2(cat,dog):
+    print (cat + dog)
+
+func2(10,20)
+i=1
+while (i<10):
+    i=i+1
+    print (i)
+
