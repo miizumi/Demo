f5bad202e3b20b3ab2b888a191066a70116ce683
miizumi <adsl4380@gmail.com>
2021-12-17 00:46:47 +0800

    method

--- C#.cs
+++ C#.cs
@@ -1,5 +1,25 @@
-using Internal;
 using System;
-int iii = 0;
-String str = "apple";
-Console.writeline(str);
+					
+public class Program
+{
+	public static void Main()
+	{
+		double a;
+		double b;
+		double c;
+		Console.WriteLine("請輸入三個數的值：");
+		a = Convert.ToDouble(Console.ReadLine());
+		b = Convert.ToDouble(Console.ReadLine());
+		c = Convert.ToDouble(Console.ReadLine());
+		if (Math.Pow(a,2)-4*a*c>=0)
+			{
+			  Console.WriteLine("x1=" + (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a);
+			  Console.WriteLine("x2=" + (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a);
+			}
+			 else
+				{
+				  Console.WriteLine("此方程無解！！！");
+				}
+				Console.ReadKey();
+	}
+}
---
+++ Note/C#/Method.md
@@ -0,0 +1,12 @@
+# Method 方法
+
+```Csharp
+string isMethod (string word)
+{
+    return "This is "+word;
+}
+```
+
+public / private
+
+void : 使方法不回傳值
